1865c024f32061dca7d8044fb6df2ca3dbeb6252
Eberle <eberle@iastate.edu>
2021-04-06 15:04:35 -0500

    delete

--- carousel.js
+++
@@ -1,8 +0,0 @@
-array = [document.getElementById("project1"), document.getElementById("project2"), document.getElementById("project3"), document.getElementById("project4"), document.getElementById("project5")];
-
-function carousel(){
-	alert("hello")
-	for (i=0, i<array.length(), i++){
-		array[i].style.left-=310;
-	}
-}
